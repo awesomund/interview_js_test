6be56a92cca63e36423dd59154117a4babf9ab63
awesomund <osmund@iterate.no>
2014-07-31 14:38:41 +0200

    added blank file with luhn-assignments ready for potential interview use

---
+++ test/luhn_blank.js
@@ -0,0 +1,58 @@
+// This is an alternative credit card validation case, using the Luhn-formula:
+//
+// Implement each of the following steps as its own method (using TDD), and then refactor them into one final validator method:
+
+// Some input validation requirements:
+// a) The credit card should not be null
+// b) The credit card number should only contain digits
+// c) The credit card number should contain 13 or more digts
+// d) The credit card number should contain 16 or less digts
+
+// Luhn algorithm:
+// 1. Drop the last digit from the number. The last digit is what we want to check against
+// 2. Reverse the numbers
+// 3. Multiply the digits in odd positions (1, 3, 5, etc.) by 2 and subtract 9 to any result higher than 9. Positions are not 0-indexed.
+//    Alternative description: multiply all even positioned digits by 2 and subtract 9 if the result is greater than 9. Positions are 0-indexed.
+// 4. Add all the numbers together
+// 5. The check digit (the last number of the card) is the amount that you would need to add to get a multiple of 10 (Modulo 10)
+
+var assert = require("assert");
+
+var creditCardNumber = "4556705936228745";
+
+// input validation methods
+
+var isNotNull = function(creditCardNumber){
+	return creditCardNumber !== null;
+};
+
+// Luhn methods
+
+//Tests
+
+describe('Credit Card Validation', function(){
+
+	describe('the credit card number', function(){
+		it('should not be empty', function(){
+			assert.equal(isNotNull(creditCardNumber), true);
+		});
+	});
+
+	// describe('the Luhn algorithm', function(){
+
+	// 	it('should drop the last digit of the credit card number', function(){
+	// 		testCreditCardNumber = "4485705063868244";
+	// 		assert.equal(removeLastDigit(testCreditCardNumber), "448570506386824");
+	// 	});
+
+	// });
+
+	// describe('the isCreditCardNumberValid() method', function(){
+
+	// 	it('should take a credit card number, put it through all the Luhn-steps and yield the correct result', function(){
+	// 		testCreditCardNumber = "4556705936228745";
+	// 		assert.equal(isCreditCardNumberValid(testCreditCardNumber), true);
+	// 	});
+	// });
+
+});
